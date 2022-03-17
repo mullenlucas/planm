9e197481684117be6c1b76abce8a5355fd73804a
mullenslucas <mullenlucas@gmail.com>
2022-03-16 23:42:34 -0300

    updatis

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="grid-container">
+    <header className="row">
+        <div className="logobrand">
+            <a href="/">planM!</a>
+        </div>
+        <nav>
+            <ul className="nav-links">
+                <li><a href="/product">Productos</a></li>
+                <li><a href="/signin">Inciar sesión</a></li>
+                <li><a href="/nice">NICE</a></li>
+            </ul>
+        </nav>
+        <a className="cta" href="/cart"><button>Carrito</button></a>
+    </header>
+    <main>
+
+    </main>
+    <footer className="row center">
+        All rights reserved
+    </footer>
+    </section>
   );
 }
 
